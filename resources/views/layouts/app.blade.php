30ddcf393aac18f7198b8b97fcd0cd1d7281c038
kevinwanyama <56598203+kevinwanyama@users.noreply.github.com>
2021-05-19 00:34:18 +0300

    added form
    design add Idea form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans bg-gray-background text-gray-900 text-sm" style="background-color:gray" >
+    <body class="font-sans bg-gray-300 text-gray-900 text-sm"  >
         <header class="flex items-center justify-between" px-8 py-4>
           <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
 
@@ -50,7 +50,66 @@
             </header>
             <main class="container mx-auto max-w-custom flex" >
                  <div class="w-70 mr-5">
-            It is often helpful to have different configuration values based on the environment where the application is running. For example, you may wish to use a different cache driver locally than you do on your production server.
+                 <div class="bg-white border-2 border-blue rounded-xl mt-16"
+                 style="
+                      border-image-source:linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba( 99, 123, 255, 0));
+                      border-image-slice:1;
+                      background-image-slice: 1;
+                      background-image:linear-gradient(to bottom, #ffffff,#ffffff), linear-gradient(to bottom,rgba(50, 138, 241, 0.22),rgba(99, 123, 255, 0));
+                      background-origin:border-box;
+                      background-clip:content-box,border-box;
+                 "
+                 >
+                     <div class="text-center px-6 py-2 pt-6">
+                         <h3 class="font-semibold text-base">Add an Idea</h3>
+                         <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                     </div>
+                     <form action="#" method="POST" class="space-y-4  px-4 py-6">
+                         <div>
+                             <input type="text" class="w-full text-sm bg-gray-100 rounded-xl border-none placeholder-gray-900 px-4 py-2"
+                             placeholder="Your Idea">
+                         </div>
+
+                         <div>
+                            <select name="category_add" id="category_add" class= "w-full bg-gray-100 text-sm border-none rounded-xl px-4 py-2">
+                                 <option value="category one">category one</option>
+                                 <option value="category two">category two</option>
+                                 <option value="category three">category three</option>
+                                 <option value="category four">category four</option>
+                                 <option value="category five">category five</option>
+                            </select>
+                        </div>
+                        <div>
+                            <textarea name="idea" id="idea" cols="30" rows="4" class="w-full bg-gray-100 rounded-xl 
+                            placeholder-gray-900 border-none text-sm px-4 py-2" placeholder="Describe your idea"></textarea>
+                        </div>
+                        <div class="flex item-center justify-between space-x-3">
+                            <button 
+                                type="button"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200
+                                font-semibold rounded-xl border border-gray-200 hover:border-gray-400
+                                transition duration-150 ease-in px-6 py-3"
+                                >
+                                <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                  </svg>
+
+                                <span class="ml-2">Attach</span>
+                            </button>
+                            <button 
+                                type="submit"
+                                class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue-400 text-white
+                                font-semibold rounded-xl border border-blue-400 hover:bg-blue-hover
+                                transition duration-150 ease-in px-6 py-3"
+                                >
+                                <span class="ml-2">Submit</span>
+                            </button>
+
+                        </div>
+
+                     </form>
+
+                 </div>
                  </div>
 
                  <div class="w-175">
