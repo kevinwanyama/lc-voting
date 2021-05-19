ff10b80adce68d4ccfe12b3b606dbeb6851d37b5
kevinwanyama <56598203+kevinwanyama@users.noreply.github.com>
2021-05-19 23:42:00 +0300

    setting status dialogs
    The dialog box that appear on pressing reply button and status button on single idea page

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,8 @@ module.exports = {
                 22:'5.5rem',
                 44:'11rem',
                 70:'17.5rem',
+                76:'19rem',
+                104:'24rem',
                 175:'43.75rem',
             },
             maxWidth:{
