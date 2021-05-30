1a3dfd9eef14ce4eb1bc3fda04e46d014aa0ba26
kevinwanyama <56598203+kevinwanyama@users.noreply.github.com>
2021-05-31 02:23:22 +0300

    using live wire to fetch ideas

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -18,16 +18,7 @@ class IdeaController extends Controller
         
         // $ideas = Idea::simplePaginate(Idea::PAGINATION_COUNT);
         // return view('idea.index', compact('ideas'));
-        return view('idea.index',[
-            'ideas'=>Idea::with('user','category','status')
-            ->addSelect(['voted_by_user' => Vote::select('id')
-                    ->where('user_id', auth()->id())
-                    ->whereColumn('idea_id', 'ideas.id')
-                ])
-            ->withCount('votes')
-            ->orderBy('id', 'desc')
-            ->simplePaginate(Idea::PAGINATION_COUNT),
-        ]);
+        return view('idea.index');
     }
 
     /**
