a8ddfdd5564e91941987807151699776e55424e9
kevinwanyama <56598203+kevinwanyama@users.noreply.github.com>
2021-05-31 01:26:57 +0300

    fixing back button when voting

--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Exceptions\DuplicateVoteException;
+use App\Exceptions\VoteNotFoundException;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -62,7 +64,33 @@ class IdeaTest extends TestCase
           $idea->vote($user);
           $this->assertTrue($idea->isVotedByUser($user));
       }
-  
+      
+      /** @test */
+    public function voting_for_an_idea_thats_already_voted_for_throws_exception()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+        ]);
+
+        $this->expectException(DuplicateVoteException::class);
+
+        $idea->vote($user);
+    }
       /** @test */
       public function user_can_remove_vote_for_idea()
       {
@@ -89,4 +117,28 @@ class IdeaTest extends TestCase
           $idea->removeVote($user);
           $this->assertFalse($idea->isVotedByUser($user));
       }
+
+
+
+       /** @test */
+    public function removing_a_vote_that_doesnt_exist_throws_exception()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea',
+        ]);
+
+        $this->expectException(VoteNotFoundException::class);
+
+        $idea->removeVote($user);
+    }
 }
